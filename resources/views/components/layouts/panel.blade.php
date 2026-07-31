e270ca3e84160fb00558a020301490a9f1ef0d77
Test User <test@test.com>
2026-07-31 14:24:37 +0300

    Widen the search box

--- resources/views/components/layouts/panel.blade.php
+++ resources/views/components/layouts/panel.blade.php
@@ -60,7 +60,7 @@
                 </div>
 
                 {{-- Search --}}
-                <div class="hidden sm:block relative w-48 lg:w-56">
+                <div class="hidden sm:block relative w-64 lg:w-[450px]">
                     <input type="text" placeholder="Arama Yap" class="w-full pl-3 pr-8 py-[5px] bg-white border border-gray-300 rounded text-[13px] text-gray-600 placeholder-gray-400 focus:outline-none">
                     <svg class="absolute right-2.5 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/></svg>
                 </div>
